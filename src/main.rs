7a6f648fe972126e962c003b86ec68272ca8f9c3
kotty342 <aa21007@cc.it-hiroshima.ac.jp>
2024-12-09 13:05:29 +0000

    初期設定: 新しい「guessing_game」プロジェクトを作成し、依存関係を追加

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");          // 数を当ててごらん
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");   // ほら、予想を入力してね
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");     // 行の読み込みに失敗しました
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);     // 次のように予想しました: {}
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),  // 小さすぎます！
+            Ordering::Greater => println!("Too big!"), // 大きすぎます！
+            Ordering::Equal => {
+                println!("You win!");  // 勝ちました！
+                break;
+            }
+        }
+    }
+}
